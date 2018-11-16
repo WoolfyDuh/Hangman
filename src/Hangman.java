1097e80b3c79e12fcf32ce54b6feb0058af7b874
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2018-11-16 20:36:25 +0100

    Initial commit

---
+++ src/Hangman.java
@@ -0,0 +1,148 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+public class Hangman {
+
+    private static String[] words = {"life", "lemmons", "love", "iq", "mediacollege", "leanderkruiden", "oliebollen", "makker", "zeg", "random", "bureau", "beeldscherm"};
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+    private static int count = 0;
+    private static String s;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextLine();
+
+        System.out.println("Dit is mijn hangman spel");
+        System.out.println(" ");
+        System.out.println("How to play:");
+        System.out.println("-je krijgt een woord");
+        System.out.println("-je moet het woord gaan gokken");
+        System.out.println("-als je alle letters goed gokt dan win je!");
+        System.out.println("-als je genoeg keren iets fout gcokt, dan verloor je");
+        System.out.println("Good luck have fun!");
+        System.out.println(" ");
+        while (count < 7 && asterisk.contains("*")) {
+			if(s.equals("kamikazi")) {System.exit(0);}
+            System.out.println("raad een letter AUB!!");
+            System.out.println(asterisk);
+            String guess = sc.next();
+            hang(guess);
+        }
+        sc.close();
+
+    }
+
+
+
+	public static void hang(String guess) {
+		String newasterisk = "";
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				newasterisk += guess.charAt(0);
+			} else if (asterisk.charAt(i) != '*') {
+				newasterisk += word.charAt(i);
+			} else {
+				newasterisk += "*";
+			}
+		}
+
+		if (asterisk.equals(newasterisk)) {
+			count++;
+			hangmanImage();
+		} else {
+			asterisk = newasterisk;
+		}
+		if (asterisk.equals(word)) {
+			System.out.println("Je hebt gewonnen!!! " + word);
+		}
+	}
+
+	public static void hangmanImage() {
+		if (count == 1) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("___|___");
+			System.out.println();
+		}
+		if (count == 2) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 3) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println("   ____________");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (count == 4) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 5) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 6) {
+			System.out.println("FOUT, Probeer een andere letter");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+		}
+		if (count == 7) {
+			System.out.println("VERLOREN");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |          _|_");
+			System.out.println("   |         / | \\");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+			System.out.println("VERLOREN! het woord was " + word);
+		}
+	}
+}
